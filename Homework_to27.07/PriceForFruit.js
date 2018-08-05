fa68e55c6dddfc431fe84e2a49c0ff5c0ec445de
Ilia Volkov <ilia.volkov@sagaris-soft.com>
2018-08-05 15:48:48 +0300

    change let, get new checked

--- Homework_to27.07/PriceForFruit.js
+++ Homework_to27.07/PriceForFruit.js
@@ -1,16 +1,32 @@
- 
 
-var priceBuy, sellOnlyToday, result, orderFruit;
-var dnoComment;
+
+var  result;
 
 function multiply(){
-    orderFruit = document.getElementById('orderFruit').value;
+   let orderFruit = document.getElementById('orderFruit').value;
         
-    priceBuy = document.getElementById('priceBuy').value;
-    priceBuy = parseInt (priceBuy, 10);
-    sellOnlyToday = document.getElementById ('sellOnlyToday').value;
-    sellOnlyToday =  parseInt (sellOnlyToday, 10);
-    result = priceBuy+(priceBuy*(sellOnlyToday/100));
-    dnoComment = (result<=priceBuy)? '\n Благотворительность? Жить за что будешь...': '\n Заработаешь хоть на пиво...' ;
-    document.getElementById('out').innerHTML = orderFruit +' ' + result + ' гривен' + dnoComment;
+   let priceBuy = document.getElementById('priceBuy').value;
+    priceBuy = parseFloat (priceBuy, 10);
+   let sellOnlyToday = document.getElementById ('sellOnlyToday').value;
+    sellOnlyToday =  parseFloat (sellOnlyToday, 10);
+        if(isNaN(sellOnlyToday)){
+        result = "Что - то пошло не так"
+        alert('ВВедите число! в пункте "Сколько нацениваем?"')
+        document.getElementById('out').innerHTML = result;
+        }
+        if(isNaN(priceBuy)){
+            result = "Что - то пошло не так"
+            alert('ВВедите число! в пункте "Почем взял?"')
+            document.getElementById('out').innerHTML = result;
+        }
+        else {
+            result = priceBuy+(priceBuy*(sellOnlyToday/100));
+            result = result.toFixed(2)
+          let  dnoComment = (result<=priceBuy)? '\n Благотворительность? Жить за что будешь...': '\n Заработаешь хоть на пиво...' ;
+            document.getElementById('out').innerHTML = orderFruit +'\n ' + result + ' гривен' + dnoComment;
+        };
+
+    
+    
+   
 }
