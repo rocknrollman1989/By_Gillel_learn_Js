cd9aee6f3a9c3e9eecd711c714986b6738b0a061
Ilia Volkov <ilia.volkov@sagaris-soft.com>
2018-08-05 13:22:41 +0300

    chacge FirstStep

--- FirstStep.js
+++ FirstStep.js
@@ -2,7 +2,7 @@ var string = 'someWord';
 var number = 2;
 
 var lokingFor2= string - number; 
-var lokingFor= string + number;
+var lokingFor= +(string + number);
 
 console.log(lokingFor2);
 console.log(lokingFor);
